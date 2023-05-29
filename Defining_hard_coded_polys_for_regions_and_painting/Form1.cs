b8b8d522cacb795a3902755c13bf74786e18bfa5
Kulpot <kulpotwahu@gmail.com>
2023-05-30 06:07:39 +0800

    Add project files.

---
+++ Defining_hard_coded_polys_for_regions_and_painting/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Defining_hard_coded_polys_for_regions_and_painting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //DrawDiamond(e.Graphics, this.ClientRectangle);
+            DrawArrow(e.Graphics, this.ClientRectangle);
+        }
+
+        private static void DrawDiamond(Graphics graphics, Rectangle rect)
+        {
+            Point[] points = new Point[]
+            {
+                new Point(rect.Width / 2, 0),       // Upper-middle
+                new Point(rect.Width, rect.Height / 2),
+                new Point(rect.Width / 2, rect.Height), // Lower-middle
+                new Point(0, rect.Height / 2)
+            };
+
+            graphics.FillPolygon(Brushes.Red, points);
+        }
+
+        private static void DrawArrow(Graphics graphics, Rectangle rect)
+        {
+            PointF[] points = new PointF[]
+            {
+                new PointF(rect.X + rect.Width * .5f, rect.Y),
+                new PointF(rect.X + rect.Width, rect.Y + rect.Height *  .4f),
+                new PointF(rect.X + rect.Width *  .7f, rect.Y + rect.Height *  .4f),
+                new PointF(rect.X + rect.Width *  .7f, rect.Y + rect.Height),
+                new PointF(rect.X + rect.Width *  .3f, rect.Y + rect.Height),
+                new PointF(rect.X + rect.Width *  .3f, rect.Y + rect.Height *  .4f),
+                new PointF(rect.X, rect.Y + rect.Height *  .4f)
+            };
+
+            graphics.FillPolygon(Brushes.Red, points);
+        }
+    }
+}
---
+++ Defining_hard_coded_polys_for_regions_and_painting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Defining_hard_coded_polys_for_regions_and_painting
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
